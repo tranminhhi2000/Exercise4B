e18bf89292a1d8f39eb5987f161d9c7168903564
Tran Minh Hi <tranminhhi2732000@gmail.com>
2019-10-07 13:57:17 +0700

    Homework

---
+++ Application4B/ConsoleApplication1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4b
+{
+
+    public class Drawing
+    {
+        private static void Setup(char[,] arr, int n)
+        {
+            for (int d = 0; d < n; d++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    arr[d, c] = ' ';
+                }
+            }
+        }
+        private static void Print(char[,] arr, int n)
+        {
+            for (int d = 0; d < n; d++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    Console.Write(" " + arr[d, c]);
+                }
+            } Console.WriteLine();
+        }
+        public static void drawU(int n)
+        {
+            char[,] arr = new char[n, n];
+            Setup(arr, n);
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i, 0] = 'u';
+                arr[i, n - 1] = 'u';
+                arr[n - 1, i] = 'u';
+
+            }
+            Print(arr, n + 1/2);
+        }
+        public static void drawW(int n)
+        {
+            char[,] arr = new char[n * 2, n * 2];
+            Setup(arr, 2 * n);
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i, i / 2] = 'W';
+
+                arr[i, n + i / 2] = 'W';
+
+                arr[i, n - 1 - i / 2] = 'W';
+
+                arr[i, n + n - 1 - i / 2] = 'W';
+            }
+            Print(arr, 2 * n);
+        }
+    }
+}
---
+++ Application4B/ConsoleApplication1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise4b
+{
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[10];
+            //InputValue(array);
+            Console.WriteLine();
+            Drawing.drawU(20);
+            //Drawing.drawW(12);
+            Console.ReadKey();
+
+
+        }
+        public static void InputValue(int[] iArr)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                Console.WriteLine("A[{0}] = ", i);
+                String str = Console.ReadLine();
+                iArr[i] = int.Parse(str);
+            }
+        }
+        public static bool IsContrain5(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                    return true;
+            }
+            return false;
+        }
+        public static bool IsContrain5or6(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5 || arr[i] == 6)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsContrain5and6(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == 5 && arr[j] == 6)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static int count5(int[] arr)
+        {
+            int c = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                    c++;
+            }
+            return c;
+        }
+        public static int count5or6(int[] arr)
+        {
+            int c = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5 || arr[i] == 6)
+                    c++;
+            }
+            return c;
+        }
+        public static int Sum(int[] arr)
+        {
+            int Sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Sum = Sum + arr[i];
+            }
+            return Sum;
+        }
+
+    }
+}
